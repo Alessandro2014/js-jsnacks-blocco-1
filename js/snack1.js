f9853c0f391616dd01f0f47754cfda1cee9e513f
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-07 17:22:47 +0200

    somma con "while"

--- js/snack1.js
+++ js/snack1.js
@@ -3,10 +3,13 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
  */
 
+        //  FOR
+
 var exsercise = document.getElementById("exsercise");
 
 var sum = 0;
-// richiedere per 5 volte un numero
+        // richiedere per 5 volte un numero
+/*
  for (var i = 0; i < 5; i++) {
     var number = parseInt(prompt("Inserisci un numero","1"));
     if (!isNaN(number)) {
@@ -16,5 +19,21 @@ var sum = 0;
         alert("non hai inserito dei numeri");
     }
 }
-//stampa su html
-exsercise.innerHTML = "la somma dei numeri da te inseriti e: " + sum;
+*/
+
+
+        //  WHILE
+var i = 0;
+while(i < 5){
+    var number = parseInt(prompt("Inserisci un numero","1"));
+    if (!isNaN(number)) {
+    //somma dei numeri inseriti 
+        sum += number;
+    }  else {
+        alert("non hai inserito dei numeri");
+    }
+    i++;
+}
+
+//STAMPA SU HTML
+exsercise.innerHTML = "La somma dei numeri da te inseriti e: " + sum;
