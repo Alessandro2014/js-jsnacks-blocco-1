52485d774bb736039090556f00fde0014f2330bd
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-07 17:50:29 +0200

    snack 2

---
+++ js/snack2.js
@@ -0,0 +1,21 @@
+/*Chiedi un numero all'utente, se è pari stampa il numero,
+se è dispari stampa il numero successivo. */
+
+var exsercise = document.getElementById("exsercise");
+// dichiaro variabile che parte da 0
+
+var number = 0;
+
+//richiesta numero
+do {
+    number = parseInt(prompt("Inserisci un numero"));
+} while (isNaN(number) || number <= 0);
+
+//divisione del numero per un numero pari e stampa risultato
+if (number % 2 === 0) {
+    exsercise.innerHTML = "Il numero è: " + number;
+}   else {
+    var sum = number + 1;
+    exsercise.innerHTML = "Il numero pari successivo al tuo è: " + sum;
+}
+
